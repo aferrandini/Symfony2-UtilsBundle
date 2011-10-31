d0b12966933b04f1c7898f05eda923b4d3aeb547
Ariel Ferrandini <ariel@ferrandini.com>
2011-10-31 16:28:15 +0100

    Added the test for isPointInsideTheCircle

--- Tests/DistanceTest.php
+++ Tests/DistanceTest.php
@@ -8,9 +8,9 @@ use Ferrandini\UtilsBundle\Lib\Distance\Coordinates;
 class DistanceTest extends WebTestCase
 {
     /**
-     * @todo Write tests
+     * Test the service method fromAtoB
      */
-    public function testService()
+    public function testFromAToB()
     {
         $client = static::createClient();
         $distance = $client->getContainer()->get('ferrandini_utils.distance');
@@ -20,4 +20,27 @@ class DistanceTest extends WebTestCase
         $this->assertEquals(157241.8159, $fromAtoB);
     }
 
+    /**
+     * Test the service method isPointInsideTheCircle
+     */
+    public function testIsPointInsideTheCircle()
+    {
+        $client = static::createClient();
+        $distance = $client->getContainer()->get('ferrandini_utils.distance');
+
+        $outside = $distance->isPointInsideTheCircle(
+            new Coordinates(0, 0),
+            new Coordinates(1, 1),
+            1000
+        );
+
+        $inside = $distance->isPointInsideTheCircle(
+            new Coordinates(0, 0),
+            new Coordinates(1, 1),
+            200000
+        );
+
+        $this->assertEquals(false, $outside);
+        $this->assertEquals(true,  $inside);
+    }
 }
