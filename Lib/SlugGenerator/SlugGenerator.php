6f67821209e2d1c7b9c03150098f1e28c62b032b
Ariel Ferrandini <aferrandini@neosistec.com>
2011-11-07 13:02:27 +0100

    SlugGenerator

---
+++ Lib/SlugGenerator/SlugGenerator.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Create by
+ * User: aferrandini
+ * Date: 07/11/11
+ * Time: 12:33
+ *
+ */
+
+namespace Ferrandini\UtilsBundle\Lib\SlugGenerator;
+
+class SlugGenerator {
+
+    /**
+     * @param string $url
+     * @param int $length
+     *
+     * @return string
+     */
+    public function generate($url, int $length=50)
+    {
+        $slug = strtolower($url);
+        $slug = preg_replace("/[^a-z0-9\s-]/", "", $slug);
+        $slug = trim(preg_replace("/\s+/", " ", $slug));
+        $slug = trim(substr($slug, 0, $length));
+        $slug = preg_replace("/\s/", "-", $slug);
+
+        return $slug;
+    }
+
+}
---
+++ Tests/SlugGeneratorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ferrandini\UtilsBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SlugGeneratorTest extends WebTestCase
+{
+    /**
+     * Test the service method Generator
+     */
+    public function testGenerator()
+    {
+        $client = static::createClient();
+        $slugger = $client->getContainer()->get('ferrandini_utils.slugger');
+
+        $slug = $slugger->generate("Nuevos Sistemas Tecnológicos S.L.");
+
+        $this->assertEquals("nuevos-sistemas-tecnologicos-sl", $slug);
+    }
+
+    /**
+     * Test the service method Generator with length
+     */
+    public function testGeneratorWithLength()
+    {
+        $client = static::createClient();
+        $slugger = $client->getContainer()->get('ferrandini_utils.slugger');
+
+        $slug = $slugger->generate("Nuevos Sistemas Tecnológicos S.L.", 10);
+
+        $this->assertEquals("nuevos-sis", $slug);
+    }
+
+}
