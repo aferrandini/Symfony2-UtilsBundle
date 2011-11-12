8fc8751613f5fccad871a8a62bcd9b922c69d794
Ariel Ferrandini <ariel@ferrandini.com>
2011-11-12 12:06:45 +0100

    Resolved issue not imported Reference

--- DependencyInjection/FerrandiniUtilsExtension.php
+++ DependencyInjection/FerrandiniUtilsExtension.php
@@ -7,6 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * This is the class that loads and manages your bundle configuration
